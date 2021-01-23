214f1c28f7d13da2a6032c85b936d585f9382bb3
Hunter KIevet <hkievet@gmail.com>
2021-01-22 16:33:44 -0800

    heck yeah get that auth to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { setConstantValue } from "typescript";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// I guess this is just a global variable that should be expected...
+const firebaseConfig = {
+  apiKey: "AIzaSyAUWOwDMPIHIrk1LTFHCCT1Wl5GP5ypyhw",
+  authDomain: "todo-d3b49.firebaseapp.com",
+  projectId: "todo-d3b49",
+  storageBucket: "todo-d3b49.appspot.com",
+  messagingSenderId: "1048297251395",
+  appId: "1:1048297251395:web:358458129485ac43f82af6",
+  measurementId: "G-9RYB2WLCSS",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+const provider = new firebase.auth.GithubAuthProvider();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [loading, setLoading] = React.useState(true);
+  const [user, setUser] = React.useState<any>();
+  React.useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      // detaching the listener
+      if (user) {
+        // ...your code to handle authenticated users.
+        setUser(user);
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe(); // unsubscribing from the listener when the component is unmounting.
+  }, []);
+
+  if (loading) {
+    return <p>Loading</p>;
+  }
+
+  if (user) {
+    // User is signed in.
+    return (
+      <>
+        <p>LOGGED IN</p>
+        <button
+          onClick={() => {
+            firebase
+              .auth()
+              .signOut()
+              .then(() => {
+                setUser(null);
+              })
+              .catch((e) => {
+                alert("error loggin out.  Sorry no ideah why.");
+              });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Log out
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            firebase
+              .auth()
+              .signInWithPopup(provider)
+              .then((result) => {
+                var credential = result.credential;
+                // The signed-in user info.
+                var user = result.user;
+                setUser(user);
+              })
+              .catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+              });
+          }}
+        >
+          Login with GitHub
+        </button>
+      </div>
+    );
+
+    // No user is signed in.
+  }
 }
 
 export default App;
