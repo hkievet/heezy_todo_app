e56283ca524e7a7f003814f7f1bc29f3efcd5708
Hunter KIevet <hkievet@gmail.com>
2021-01-25 20:46:34 -0800

    remove the unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-import { setConstantValue } from "typescript";
 import TodoApp from "./todos/TodoApp";
 import { AppContext, IAppContext } from "./AppContext";
 import {
